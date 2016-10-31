15fa447c1db2a01505b5ef1cbcb3b70d573b55db
Mauro Buccioli <maurik77@hotmail.com>
2016-10-31 18:24:40 +0100

    Started

---
+++ MS.SF.RS.Caching.Interfaces/IDistribuitedCache.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.SF.RS.Caching.Interfaces
+{
+    public interface IDistribuitedCache : IService
+    {
+        object GetAsync(string region, string key);
+
+        void SetOrUpdateAsync(string region, string key, string obj, Entities.CacheItemPolicy policy);
+
+        void UpdatePolicyAsync(string region, string key, Entities.CacheItemPolicy policy);
+
+        void DeleteAsync(string region, string key);
+
+        void CleanAsync(string region);
+
+        void CleanAllAsync();
+    }
+}
